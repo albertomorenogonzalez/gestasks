114e4c3f0c0e17151ffee8f20c96b43cfa02c2ef
Alberto <albertomorenogonzalez03@gmail.com>
2022-10-11 11:04:09 +0200

    La página home ya no está siempre seleccionada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home.page.html',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
+    path: 'gestpeople',
+    loadChildren: () => import('./pages/gestpeople/gestpeople.module').then( m => m.GestpeoplePageModule)
+  },
+  {
+    path: 'gesttasks',
+    loadChildren: () => import('./pages/gesttasks/gesttasks.module').then( m => m.GesttasksPageModule)
+  },
+  {
+    path: 'asigntasks',
+    loadChildren: () => import('./pages/asigntasks/asigntasks.module').then( m => m.AsigntasksPageModule)
+  },
+  {
+    path: 'seetasks',
+    loadChildren: () => import('./pages/seetasks/seetasks.module').then( m => m.SeetasksPageModule)
+  },
+  {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: '/home', icon: 'home' },
-    { title: 'Gestionar Personas', url: '/home/gestpeople', icon: 'people' },
-    { title: 'Gestionar Tareas', url: '/home/gesttasks', icon: 'create' },
-    { title: 'Asignar Tareas', url: '/home/asigntasks', icon: 'person-add' },
-    { title: 'Ver Tareas Asignadas', url: '/home/seetasks', icon: 'file-tray-full' },
+    { title: 'Gestionar Personas', url: '/gestpeople', icon: 'people' },
+    { title: 'Gestionar Tareas', url: '/gesttasks', icon: 'create' },
+    { title: 'Asignar Tareas', url: '/asigntasks', icon: 'person-add' },
+    { title: 'Ver Tareas Asignadas', url: '/seetasks', icon: 'file-tray-full' }
   ];
-
-  //La pestaña home está permanentemente seleccionada debido a que el resto de páginas están dentro de la página home (buscar forma de solucionarlo)
   
   constructor() {}
 }
