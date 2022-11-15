73129d136f3b9b9411fe3757ffd9d205157e704f
Alberto <albertomorenogonzalez03@gmail.com>
2022-11-15 21:12:33 +0100

    Importados los idiomas español e inglés, español como predeterminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +6,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(es)
+registerLocaleData(en)
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LOCALE_ID,
+      useValue: 'es'}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
